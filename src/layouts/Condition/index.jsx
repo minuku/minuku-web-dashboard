0486035d1c99f5a93e12c4890343daa32610d983
Andy <andy@Andyde-MacBook-Pro.local>
2018-06-29 22:59:40 +0800

    [ADD] Add condition dialog

--- src/layouts/Condition/index.jsx
+++ src/layouts/Condition/index.jsx
@@ -5,10 +5,8 @@ import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import red from '@material-ui/core/colors/red';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import Button from '@material-ui/core/Button';
 import purple from '@material-ui/core/colors/purple';
@@ -17,12 +15,19 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
-import CommentIcon from '@material-ui/icons/Comment';
 import EditIcon from '@material-ui/icons/Edit';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import TextField from '@material-ui/core/TextField';
+import Checkbox from '@material-ui/core/Checkbox';
+import MenuItem from '@material-ui/core/MenuItem';
 
 const styles = theme => ({
   card: {
-    maxWidth: 400,
+    maxWidth: 600,
   },
   media: {
     height: 0,
@@ -31,24 +36,11 @@ const styles = theme => ({
   actions: {
     display: 'flex',
   },
-  expand: {
-    transform: 'rotate(0deg)',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
-    marginLeft: 'auto',
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)',
-  },
-  avatar: {
-    backgroundColor: red[500],
-  },
   cssRoot: {
-    color: theme.palette.getContrastText(purple[500]),
-    backgroundColor: purple[500],
+    color: theme.palette.getContrastText(purple[700]),
+    backgroundColor: purple[700],
     '&:hover': {
-      backgroundColor: purple[700],
+      backgroundColor: purple[900],
     },
   },
 });
@@ -59,12 +51,35 @@ const theme = createMuiTheme({
   },
 });
 
-class RecipeReviewCard extends React.Component {
-  state = { expanded: false };
 
-  handleExpandClick = () => {
-    this.setState({ expanded: !this.state.expanded });
+const hourInterval = Array(12).fill().map((value, index) => index + 1);
+const minInterval = Array(60).fill().map((value, index) => index + 1);
+const amPm = ['AM', 'PM'];
+const timeUnit = ['hour', 'minute', 'second'];
+
+
+
+class RecipeReviewCard extends React.Component {
+  state = { 
+    open: false,
+    name: 'condition 1',
   };
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+    //console.log("click");
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  };
+
 
   render() {
     const { classes } = this.props;
@@ -90,7 +105,7 @@ class RecipeReviewCard extends React.Component {
               >
                 <ListItemText primary={`Condition ${value + 1}`} />
                 <ListItemSecondaryAction>
-                  <IconButton aria-label="Comments">
+                  <IconButton aria-label="Comments" onClick={this.handleClickOpen}>
                     <EditIcon />
                   </IconButton>
                 </ListItemSecondaryAction>
@@ -104,41 +119,242 @@ class RecipeReviewCard extends React.Component {
                 variant="contained" 
                 color="primary" 
                 className={classes.margin}
+                onClick={this.handleClickOpen}
               >
                 + ADD
               </Button>
-      </MuiThemeProvider>
+            </MuiThemeProvider>
           </CardActions>
-          <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
-            <CardContent>
-              <Typography paragraph variant="body2">
-                Method:
-              </Typography>
-              <Typography paragraph>
-                Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-                minutes.
-              </Typography>
-              <Typography paragraph>
-                Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-                heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-                browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving
-                chicken and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion,
-                salt and pepper, and cook, stirring often until thickened and fragrant, about 10
-                minutes. Add saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-              </Typography>
-              <Typography paragraph>
-                Add rice and stir very gently to distribute. Top with artichokes and peppers, and
-                cook without stirring, until most of the liquid is absorbed, 15 to 18 minutes.
-                Reduce heat to medium-low, add reserved shrimp and mussels, tucking them down into
-                the rice, and cook again without stirring, until mussels have opened and rice is
-                just tender, 5 to 7 minutes more. (Discard any mussels that don’t open.)
-              </Typography>
-              <Typography>
-                Set aside off of the heat to let rest for 10 minutes, and then serve.
-              </Typography>
-            </CardContent>
-          </Collapse>
         </Card>
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">
+            <TextField
+              id="name"
+              label="Name"
+              className={classes.textField}
+              value={this.state.name}
+              onChange={this.handleChange('name')}
+              margin="normal"
+            />
+          </DialogTitle>
+          <DialogContent>
+            <List>
+              <ListItem
+                role={undefined}
+                dense
+                className={classes.listItem}
+              >
+                <Checkbox
+                  tabIndex={-1}
+                  disableRipple
+                />
+                <ListItemText primary='From' />
+                <TextField
+                  id="select-currency"
+                  select
+                  className={classes.textField}
+                  value={this.state.currency}
+                  onChange={this.handleChange('currency')}
+                  SelectProps={{
+                    MenuProps: {
+                      className: classes.menu,
+                    },
+                  }}
+                  margin="normal"
+                >
+                  {hourInterval.map(option => (
+                    <MenuItem key={option} value={option}>
+                      {option}
+                    </MenuItem>
+                  ))}
+                </TextField>
+                <ListItemText primary='：' />
+                <TextField
+                  id="select-currency"
+                  select
+                  className={classes.textField}
+                  value={this.state.currency}
+                  onChange={this.handleChange('currency')}
+                  SelectProps={{
+                    MenuProps: {
+                      className: classes.menu,
+                    },
+                  }}
+                  margin="normal"
+                >
+                  {minInterval.map(option => (
+                    <MenuItem key={option} value={option}>
+                      {option}
+                    </MenuItem>
+                  ))}
+                </TextField>
+                <ListItemText primary=' ' />
+                <TextField
+                  id="select-currency"
+                  select
+                  className={classes.textField}
+                  value={this.state.currency}
+                  onChange={this.handleChange('currency')}
+                  SelectProps={{
+                    MenuProps: {
+                      className: classes.menu,
+                    },
+                  }}
+                  margin="normal"
+                >
+                  {amPm.map(option => (
+                    <MenuItem key={option} value={option}>
+                      {option}
+                    </MenuItem>
+                  ))}
+                </TextField>
+                <ListItemText primary='to' />
+                <TextField
+                  id="select-currency"
+                  select
+                  className={classes.textField}
+                  value={this.state.currency}
+                  onChange={this.handleChange('currency')}
+                  SelectProps={{
+                    MenuProps: {
+                      className: classes.menu,
+                    },
+                  }}
+                  margin="normal"
+                >
+                  {hourInterval.map(option => (
+                    <MenuItem key={option} value={option}>
+                      {option}
+                    </MenuItem>
+                  ))}
+                </TextField>
+                <ListItemText primary='：' />
+                <TextField
+                  id="select-currency"
+                  select
+                  className={classes.textField}
+                  value={this.state.currency}
+                  onChange={this.handleChange('currency')}
+                  SelectProps={{
+                    MenuProps: {
+                      className: classes.menu,
+                    },
+                  }}
+                  margin="normal"
+                >
+                  {minInterval.map(option => (
+                    <MenuItem key={option} value={option}>
+                      {option}
+                    </MenuItem>
+                  ))}
+                </TextField>
+                <ListItemText primary=' ' />
+                <TextField
+                  id="select-currency"
+                  select
+                  className={classes.textField}
+                  value={this.state.currency}
+                  onChange={this.handleChange('currency')}
+                  SelectProps={{
+                    MenuProps: {
+                      className: classes.menu,
+                    },
+                  }}
+                  margin="normal"
+                >
+                  {amPm.map(option => (
+                    <MenuItem key={option} value={option}>
+                      {option}
+                    </MenuItem>
+                  ))}
+                </TextField>
+              </ListItem>
+              <ListItem
+                role={undefined}
+                dense
+                className={classes.listItem}
+              >
+                <Checkbox
+                  tabIndex={-1}
+                  disableRipple
+                />
+                <ListItemText primary='Last for' />
+                <TextField
+                  id="select-currency"
+                  select
+                  className={classes.textField}
+                  value={this.state.currency}
+                  onChange={this.handleChange('currency')}
+                  SelectProps={{
+                    MenuProps: {
+                      className: classes.menu,
+                    },
+                  }}
+                  margin="normal"
+                >
+                  {hourInterval.map(option => (
+                    <MenuItem key={option} value={option}>
+                      {option}
+                    </MenuItem>
+                  ))}
+                </TextField>
+                <ListItemText primary='：' />
+                <TextField
+                  id="select-currency"
+                  select
+                  className={classes.textField}
+                  value={this.state.currency}
+                  onChange={this.handleChange('currency')}
+                  SelectProps={{
+                    MenuProps: {
+                      className: classes.menu,
+                    },
+                  }}
+                  margin="normal"
+                >
+                  {minInterval.map(option => (
+                    <MenuItem key={option} value={option}>
+                      {option}
+                    </MenuItem>
+                  ))}
+                </TextField>
+                <ListItemText primary=' ' />
+                <TextField
+                  id="select-currency"
+                  select
+                  className={classes.textField}
+                  value={this.state.currency}
+                  onChange={this.handleChange('currency')}
+                  SelectProps={{
+                    MenuProps: {
+                      className: classes.menu,
+                    },
+                  }}
+                  margin="normal"
+                >
+                  {timeUnit.map(option => (
+                    <MenuItem key={option} value={option}>
+                      {option}
+                    </MenuItem>
+                  ))}
+                </TextField>
+              </ListItem>
+            </List>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose} color="primary">
+              cancel
+            </Button>
+            <Button onClick={this.handleClose} color="primary" autoFocus>
+              done
+            </Button>
+          </DialogActions>
+        </Dialog>
       </div>
     );
   }
