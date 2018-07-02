410f15999f0161ea9485d03160f1436d16ad263b
lichin-lin <vic20087cjimlin@gmail.com>
2018-07-03 02:17:58 +0800

    add: customize theme

--- src/index.js
+++ src/index.js
@@ -17,7 +17,9 @@ import { configureFakeBackend } from 'utils/fakeBackend'
 import registerServiceWorker from './registerServiceWorker'
 
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import { customizeTheme } from 'utils/customizeTheme'
 
+// redux data flow
 const store = createStore(
   reducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
@@ -27,13 +29,8 @@ const store = createStore(
 // setup fake backend
 configureFakeBackend()
 
-const theme = createMuiTheme({
-  typography: {
-    headline: {
-      fontSize: `1.25rem`,
-    },
-  },
-})
+// customize mui theme
+const theme = createMuiTheme(customizeTheme)
 
 ReactDOM.render(
   <MuiThemeProvider theme={theme}>
---
+++ src/utils/customizeTheme.js
@@ -0,0 +1,24 @@
+import blue from '@material-ui/core/colors/blue'
+import amber from '@material-ui/core/colors/amber'
+
+export const customizeTheme = {
+  typography: {
+    headline: {
+      fontSize: `1.25rem`,
+    },
+  },
+  palette: {
+    primary: {
+      light: blue[300],
+      main: blue[600],
+      dark: blue[800],
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: amber[300],
+      main: amber[600],
+      dark: amber[800],
+      contrastText: '#000',
+    },
+  },
+}
