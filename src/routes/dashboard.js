587aee74f5495e1dbb1dda89f56560fb0d1ac2b4
張耀天 <zhangyaotian@zhangyaotiande-MacBook-Air.local>
2018-07-04 11:21:34 +0800

    add a basic card with only title

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,4 +1,5 @@
 import ProfilePage from 'layouts/Profile/index.jsx'
+import ProjectPage from 'layouts/Project/index.jsx'
 
 const dashboardRoutes = [
   {
@@ -6,6 +7,12 @@ const dashboardRoutes = [
     sidebarName: "Profile",
     navbarName: "Profile",
     component: ProfilePage
+  },
+  {
+    path: "/dashboard/project",
+    sidebarName: "Project",
+    navbarName: "Project",
+    component: ProjectPage
   },
   // { redirect: true, path: "/dashboard", to: "/dashboard/profile", navbarName: "Redirect" }
 ]
