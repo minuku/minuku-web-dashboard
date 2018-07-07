48bef8648f9b5751ce5c53b72d84b75bf7c9db49
lichin-lin <vic20087cjimlin@gmail.com>
2018-07-07 16:27:19 +0800

    modify: using prettier to clean up the code

--- src/index.js
+++ src/index.js
@@ -1,52 +1,56 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import { history } from "utils/history"
-import { Route, Router, Switch } from "react-router-dom"
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { history } from "utils/history";
+import { Route, Router, Switch } from "react-router-dom";
 
-import thunk from 'redux-thunk'
-import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
-import reducers from './reducers'
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reducers from "./reducers";
 
-import PrivateRoute from 'containers/PrivateRoute.jsx'
-import Account from "layouts/Account/index.jsx"
-import indexRoute from 'routes'
+import PrivateRoute from "containers/PrivateRoute.jsx";
+import Account from "layouts/Account/index.jsx";
+import indexRoute from "routes";
 
-import { configureFakeBackend } from 'utils/fakeBackend'
-import registerServiceWorker from './registerServiceWorker'
+import { configureFakeBackend } from "utils/fakeBackend";
+import registerServiceWorker from "./registerServiceWorker";
 
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
-import { customizeTheme } from 'utils/customizeTheme'
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { customizeTheme } from "utils/customizeTheme";
 
 // redux data flow
 const store = createStore(
   reducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(thunk)
-)
+);
 
 // setup fake backend
-configureFakeBackend()
+configureFakeBackend();
 
 // customize mui theme
-const theme = createMuiTheme(customizeTheme)
+const theme = createMuiTheme(customizeTheme);
 
 ReactDOM.render(
   <MuiThemeProvider theme={theme}>
     <Provider store={store}>
       <Router history={history}>
         <Switch>
-          {
-            indexRoute.map((prop, key) => {
-                return <PrivateRoute key={key} path={prop.path} component={prop.component} />
-            })
-          }
+          {indexRoute.map((prop, key) => {
+            return (
+              <PrivateRoute
+                key={key}
+                path={prop.path}
+                component={prop.component}
+              />
+            );
+          })}
           <Route path="/" component={Account} />
         </Switch>
       </Router>
     </Provider>
   </MuiThemeProvider>,
-  document.getElementById('root')
-)
-registerServiceWorker()
+  document.getElementById("root")
+);
+registerServiceWorker();
