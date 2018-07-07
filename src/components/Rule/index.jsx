4e873ab966fa5ace97d754ef0982e509320388f8
Andy <andy@Andyde-MacBook-Pro.local>
2018-07-07 10:54:56 +0800

    [ADD] add rule component

---
+++ src/components/Rule/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { withStyles } from '@material-ui/core/styles'
+
+class Rule extends React.Component{
+
+  render(){
+
+
+  }
+
+}
+
+export default withStyles(styles, { withTheme: true })(Rule)
