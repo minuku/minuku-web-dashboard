294b6e453cad08f27e8d520ead4c6eb31b8b5a76
張耀天 <zhangyaotian@zhangyaotiande-MacBook-Air.local>
2018-07-10 21:38:36 +0800

    MODIFY: Change Back to <Table>

--- src/layouts/Project/index.jsx
+++ src/layouts/Project/index.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
 import { withStyles } from '@material-ui/core/styles'
 import EditIcon from '@material-ui/icons/Edit';
-//import Button from '@material-ui/core/Button'
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 import Typography from '@material-ui/core/Typography'
 //import TextField from '@material-ui/core/TextField'
 import Checkbox from '@material-ui/core/Checkbox';
 import Divider from '@material-ui/core/Divider';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
 import IconButton from '@material-ui/core/IconButton';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
-// fake apiUrl
-//import { userService } from 'utils/userService'
 
 const styles = theme => ({
   root: {
@@ -63,122 +62,98 @@ class Project extends React.Component {
 
   state = {
     showingProjectId: 0,
-    PJlist: [                           //list of project
-      {
-        title: `Project A`,
-        list: [                       //list of setting
-          { id: 0,
-            checked: false,
-            action: `收集運動資料`,
-            situation: `室外運動`,
-            schedule: `每日兩次`
-          },
-          { id:1,
-            checked: false,
-            action: `發問卷`,
-            situation: `室內運動`,
-            schedule: `每日三次`
-          },
-          { id:2,
-            checked: false,
-            action: `通知Admin`,
-            situation: `進入睡眠`,
-            schedule: `每日一次`
-          },
-        ] 
+    title: `Project A`,
+    list: [                       //list of setting
+      { id: 0,
+        checked: false,
+        action: `收集運動資料`,
+        situation: `室外運動`,
+        schedule: `每日兩次`
       },
-      {
-        title: `Project B`,
-        list: [                       //list of setting
-          { id: 0,
-            checked: false,
-            action: `偵測光線`,
-            situation: `室內`,
-            schedule: `每日兩次`
-          },
-          { id:1,
-            checked: false,
-            action: `紀錄GPS座標`,
-            situation: `出門`,
-            schedule: `每日一次`
-          },
-          { id:2,
-            checked: false,
-            action: `紀錄加速度`,
-            situation: `坐上交通工具`,
-            schedule: `每日一次`
-          },
-        ] 
-      }
-    ] 
+      { id:1,
+        checked: false,
+        action: `發問卷`,
+        situation: `室內運動`,
+        schedule: `每日三次`
+      },
+      { id:2,
+        checked: false,
+        action: `通知Admin`,
+        situation: `進入睡眠`,
+        schedule: `每日一次`
+      },
+    ],
 
   } ;
 
   handleEditClick = (v) => {
     //alert(v.action);
-    v.action = `已更改`;
+    //v.action = `已更改`;
     //alert(v.action);
     alert(`Editing function is unfinished, I am working on it QQ` );
   };
 
   handleSelectClick = (v) =>{
     v.checked=!v.checked;
+    alert(`This is select click ${v.checked}`);
   };
 
   handleTest = () =>{
-    alert(4);
+    alert('handleTest, also unfinished.QQ');
   };
+
+//TO DO List: Do we need Boot-Strap-Table???
 
   render () {
     const { classes } = this.props
     
     return (
       <div className={classes.root}>
-  
-        <Grid container className={classes.root} spacing={0}>
-          <Grid item xs>
-            <Grid container className={classes.demo} direction="column" spacing={0}>
-              {[0].map(value => (
-                <Grid key={value} item >
-                  <Paper className={classes.paper} >   
-                    <List>
-                      <ListItem>
-                        <Typography className={classes.title}> {this.state.PJlist[value].title}</Typography>
-                        <ListItemSecondaryAction><IconButton><MoreVertIcon/></IconButton></ListItemSecondaryAction>
-                      </ListItem>
-                      <ListItem >
-                        <Grid container className={classes.headerlist} direction="row" spacing={0}>
-                          <Grid item xs></Grid>
-                          <Grid item xs={4}>Label</Grid>
-                          <Grid item xs={2}>Action</Grid>
-                          <Grid item xs={2}>Situation</Grid>
-                          <Grid item xs={2}>Schedule</Grid>
-                          <Grid item xs></Grid>
-                        </Grid>
-                      </ListItem>
-                      <Divider/>
-                        {projectConfig.map(val => (  
-                          <ListItem >
-                            <Grid container className={classes.headerlist} direction="row" spacing={0}>
-                              <Grid item xs> <Checkbox color="primary" onClick={() => this.handleSelectClick(val)}/> </Grid>
-                              <Grid item xs={4}></Grid>
-                              <Grid item xs={2}>{val.action}</Grid>
-                              <Grid item xs={2}>{val.situation}</Grid>
-                              <Grid item xs={2}>{val.schedule} </Grid>
-                              <Grid item xs> <ListItemSecondaryAction>
-                                <IconButton onClick={() => this.handleEditClick(val)}> <EditIcon/></IconButton>
-                              </ListItemSecondaryAction> </Grid>
-                            </Grid>
-                          </ListItem>
-                        ))}
-                    </List>
-                  </Paper>
-                </Grid>
-              ))}
-            </Grid>
-          </Grid>
-        </Grid>
+      <Grid container className={classes.root} spacing={0} direction="column">
+        {/*<Grid item xs>
+          <Typography>"Project A" </Typography>
+        </Grid>*/}
+        <Grid item xs>
+            
+          <Paper>
+          <Table className={classes.title}>
+            <TableHead>
+              <TableRow>
+                <TableCell padding="checkbox"></TableCell>
+                <TableCell ></TableCell>
+                <TableCell >Label</TableCell>
+                <TableCell padding="dense">Action</TableCell>
+                <TableCell padding="dense">Situation</TableCell>
+                <TableCell padding="dense">Schedule</TableCell>
+                <TableCell numeric padding="checkbox">
+                  <IconButton>
+                    <MoreVertIcon onClick={() => this.handleEditClick(0)} />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>      
+          {projectConfig.map(val => ( 
+              <TableRow>
+                <TableCell padding="checkbox"><Checkbox color="primary" onClick={() => this.handleSelectClick(val)}/> </TableCell>
+                <TableCell ></TableCell>
+                <TableCell ></TableCell>
+                <TableCell padding="dense">{val.action}</TableCell>
+                <TableCell padding="dense">{val.situation}</TableCell>
+                <TableCell padding="dense">{val.schedule}</TableCell>
+                <TableCell numeric padding="checkbox">
+                  <IconButton>
+                  <EditIcon onClick={() => this.handleEditClick(0)} />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            ))}
+            </TableBody>
+          </Table>
+          </Paper>
         
+        </Grid>
+      </Grid>
       </div>
     )
   }
