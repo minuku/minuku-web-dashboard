a473c037fc87d1eec255558a568585f37c223ef9
張耀天 <zhangyaotian@zhangyaotiande-MacBook-Air.local>
2018-07-12 18:13:16 +0800

    ADD: HandleClicks Function 4 buttons (prototype)

--- src/layouts/Project/index.jsx
+++ src/layouts/Project/index.jsx
@@ -14,6 +14,11 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 
 const styles = theme => ({
@@ -30,32 +35,10 @@ const styles = theme => ({
 })
 
 const options = [
-  'Delete',
   'Rename',
+  'AddRule',
   'Clear',
-];
-
-var projectConfig = [
-  { 
-    id: 0,
-    checked: false,
-    action: `收集運動資料`,
-    situation: `室外運動`,
-    schedule: `每日兩次`
-  },
-  { 
-    id: 1,
-    checked: false,
-    action: `發問卷`,
-    situation: `室內運動`,
-    schedule: `每日三次`
-  },
-  { id: 2,
-    checked: false,
-    action: `通知Admin`,
-    situation: `進入睡眠`,
-    schedule: `每日一次`
-  },
+  'Delete',
 ];
 
 class Project extends React.Component {
@@ -63,6 +46,7 @@ class Project extends React.Component {
   state = {
     showingProjectId: 0,
     title: `Project A`,
+    anchorEl: null,
     list: [                       //list of setting
       { id: 0,
         checked: false,
@@ -86,16 +70,29 @@ class Project extends React.Component {
 
   } ;
 
-  handleEditClick = (v) => {
-    //alert(v.action);
-    //v.action = `已更改`;
-    //alert(v.action);
-    alert(`Editing function is unfinished, I am working on it QQ` );
+  handleMoreVetClick = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleMoreVertClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  handleProjectNameClick = (v) => {
+    this.setState({title:`Project B`});
+    alert(`This icon should change title name.` );
   };
 
   handleSelectClick = (v) =>{
     v.checked=!v.checked;
     alert(`This is select click ${v.checked}`);
+  };
+
+  handleEditClick = (v) => {
+    const temp = this.state.list ;
+    temp[v.id].action = `Changed`;
+    this.setState( { list : temp});
+    alert(`Editing ${v.id}` );
   };
 
   handleTest = () =>{
@@ -116,23 +113,34 @@ class Project extends React.Component {
         <Grid item xs>
             
           <Paper>
+            <List>
+              <ListItem>
+                <Typography className={classes.title}>{this.state.title}</Typography>
+                <ListItemSecondaryAction>
+                  <IconButton>
+                    <MoreVertIcon onClick={() => this.handleProjectNameClick(0)} />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
+            </List>
           <Table className={classes.title}>
             <TableHead>
               <TableRow>
                 <TableCell padding="checkbox"></TableCell>
-                <TableCell ></TableCell>
-                <TableCell >Label</TableCell>
+                <TableCell padding="dense"></TableCell>
+                <TableCell padding="dense">Label</TableCell>
                 <TableCell padding="dense">Action</TableCell>
                 <TableCell padding="dense">Situation</TableCell>
                 <TableCell padding="dense">Schedule</TableCell>
                 <TableCell numeric padding="checkbox">
-                  <IconButton>
+                  {/*<IconButton>
                     <MoreVertIcon onClick={() => this.handleEditClick(0)} />
-                  </IconButton>
+                  </IconButton>*/}
                 </TableCell>
               </TableRow>
             </TableHead>
-            <TableBody>      
+            <TableBody>   
+              {/*   
           {projectConfig.map(val => ( 
               <TableRow>
                 <TableCell padding="checkbox"><Checkbox color="primary" onClick={() => this.handleSelectClick(val)}/> </TableCell>
@@ -144,6 +152,22 @@ class Project extends React.Component {
                 <TableCell numeric padding="checkbox">
                   <IconButton>
                   <EditIcon onClick={() => this.handleEditClick(0)} />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            ))}
+          */}
+            {this.state.list.map(val => ( 
+              <TableRow>
+                <TableCell padding="checkbox"><Checkbox color="primary" onClick={() => this.handleSelectClick(val)}/> </TableCell>
+                <TableCell padding="dense"></TableCell>
+                <TableCell padding="dense"></TableCell>
+                <TableCell padding="dense">{val.action}</TableCell>
+                <TableCell padding="dense">{val.situation}</TableCell>
+                <TableCell padding="dense">{val.schedule}</TableCell>
+                <TableCell numeric padding="checkbox">
+                  <IconButton>
+                  <EditIcon onClick={() => this.handleEditClick(val)} />
                   </IconButton>
                 </TableCell>
               </TableRow>
