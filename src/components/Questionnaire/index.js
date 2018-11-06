11c2c5b0a58580645bc195cb6dce2d91c6864998
Yang Lin <yanglin5689446@gmail.com>
2018-11-06 22:34:45 +0800

    Add Questionnaire components

--- src/components/Questionnaire/index.js
+++ src/components/Questionnaire/index.js
@@ -1,45 +1,25 @@
 
-import React, { Component } from "react";
+import React from "react";
 
 import * as Questions from './Questions'
 import Body from './Body'
 import List from './List'
-import { withStyles } from "@material-ui/core/styles";
-// import { consolidateStreamedStyles } from "styled-components";
 
-const styles = theme => ({
-  card: {
-    display: 'flex',
-    justifyContent: 'center',
-    [theme.breakpoints.down("sm")]: {
-      flexDirection: 'column',
-    }
-  }
-})
-
-class Questionnaire extends Component {
-  render() {
-    const { classes } = this.props;
-    return(
-      <div className={classes.card}>
-        <div>
-          <Body>
-            <Questions.ShortText />
-            <Questions.LongText />
-            <Questions.MultipleChoice />
-            <Questions.MatrixQuestions />
-            <Questions.Statement />
-          </Body>
-        </div>
-      <div className="pt-4">
-        <List/>
-      </div>
+const Questionnaire = () => (
+  <div className="d-flex justify-content-center">
+    <div className="px-4">
+      <Body>
+        <Questions.ShortText />
+        <Questions.LongText />
+        <Questions.MultipleChoice />
+        <Questions.MatrixQuestions />
+        <Questions.Statement />
+      </Body>
     </div>
-    )
-  }
-  
-}
- 
+    <div className="pt-4">
+      <List/>
+    </div>
+  </div>
+)
 
-
-export default withStyles(styles)(Questionnaire);
+export default Questionnaire
